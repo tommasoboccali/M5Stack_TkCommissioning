cb28a83b7326537d97a2a48237a039ae00e520dc
tommasoboccali <tommaso.boccali@cern.ch>
2021-02-19 17:15:14 +0100

    measurements from feb 19th

--- Analysis/Feb_19th_2021.py
+++ Analysis/Feb_19th_2021.py
@@ -1,18 +1,21 @@
 from numpy import genfromtxt
 import matplotlib.pyplot as plt
-my_data = genfromtxt('../Data/Feb_18th_2021.txt', delimiter=' ')
+my_data = genfromtxt('../Data/Feb_19th_2021.txt', delimiter=' ')
 mystates = {"Off":0, "Initialized":1, "Ready":2,"HeatingOn":3, "HeatingOff":4 ,"DumpResults":5}
 my_curated_data = my_data[:,[0,4,8,9,10,11,12,13,14,15]]
 
 
 timestamp_moments= {
-    1613655383: "V=19V A=0.49A FAN laterale",
-    1613655903: "V=24V, A=0.58A",
-    1613656097: "pigio sensore 1",
-    1613656124: "pigio sensore 1",
-    1613656252: "V=0V",
-    1613656921: "metto ventola sopra, V=19V",
-    1613657085: "V=0V"
+    1613748629: "V=19V A=0.49A FAN laterale",
+    1613748994: "sensor 0 problematic",
+    1613749034: "serson 0 fixed",
+    1613749297: "sensor 0 still problems; I go all off",
+    1613749637: "V=24V A=0.59",
+    1613750162: "V=30V A=0.69A (new fan PS)",
+    1613750482: "V=24V",
+    1613750553: "Sensor 4 in the middle",
+    1613750818: "V=30V A=0.69A (new fan PS)",
+    1613751077: "all off",
     }
 
 
@@ -21,17 +24,17 @@ timestamp_moments= {
 #position in curated data
 # 2,3,4,5,6,7,8,9
 
-#typically are sensors 6,1; below sensors 3,5
+#typically are sensors 0,4; below sensors 2,7
     
-delta_T_Above_minus_below = ( (my_curated_data[:,8]+my_curated_data[:,3])-(my_curated_data[:,7]+my_curated_data[:,5]) ) / 2
+delta_T_Above_minus_below = ( (my_curated_data[:,2]+my_curated_data[:,6])-(my_curated_data[:,4]+my_curated_data[:,9]) ) / 2
     
 fig, ax = plt.subplots(2)
 
 #sensors
-ax[0].scatter(my_curated_data[:,0],my_curated_data[:,8], label = 'Sensor 6')
-ax[0].scatter(my_curated_data[:,0],my_curated_data[:,3], label = 'Sensor 1')
-ax[0].scatter(my_curated_data[:,0],my_curated_data[:,5], label = 'Sensor 3')
-ax[0].scatter(my_curated_data[:,0],my_curated_data[:,7], label = 'Sensor 5')
+ax[0].scatter(my_curated_data[:,0],my_curated_data[:,2], label = 'Sensor 0')
+ax[0].scatter(my_curated_data[:,0],my_curated_data[:,6], label = 'Sensor 4')
+ax[0].scatter(my_curated_data[:,0],my_curated_data[:,4], label = 'Sensor 2')
+ax[0].scatter(my_curated_data[:,0],my_curated_data[:,9], label = 'Sensor 7')
 ax[0].set_ylim([-10,100])
 
 trans = ax[0].get_xaxis_transform()
@@ -51,7 +54,7 @@ ax[0].grid(True)
 ax[1].scatter(my_curated_data[:,0],delta_T_Above_minus_below, label = 'Delta T above-below')
 ax[1].legend()
 ax[1].grid(True)
-ax[1].set_ylim([-2,2])
+ax[1].set_ylim([-5,5])
 
 trans = ax[1].get_xaxis_transform()
 delta = -9
